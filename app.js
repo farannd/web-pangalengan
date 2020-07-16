9a759b3c57c372cd9d4f3742528c3b7da082d80e
Farand Farhansyah <47436746+farannd@users.noreply.github.com>
2020-07-16 15:26:32 +0700

    refactoring

--- app.js
+++ app.js
@@ -78,6 +78,13 @@ app.use(userRoutes);
 app.use(touristRoutes);
 app.use(galleryRoutes);
 
+// Forward all requests to HTTPS.
+// enable reverse proxy support in Express. This causes the
+// the "X-Forwarded-Proto" header field to be trusted so its
+// value can be used to determine the protocol. See
+// http://expressjs.com/api#app-settings for more details.
+app.enable('trust proxy');
+
 // Add a handler to inspect the req.secure flag (see
 // http://expressjs.com/api#req.secure). This allows us
 // to know whether the request was via http or https.
